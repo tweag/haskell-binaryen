fc1be6729a1dcb6c6c0ea842c7de59fdc4a29a84
Shao Cheng <cheng.shao@tweag.io>
2019-12-30 13:07:09 +0100

    Add asterius-specific cbits

--- binaryen/src/binaryen-c.cpp
+++ binaryen/src/binaryen-c.cpp
@@ -953,6 +953,14 @@ BinaryenOp BinaryenWidenHighUVecI16x8ToVecI32x4(void) {
 }
 BinaryenOp BinaryenSwizzleVec8x16(void) { return SwizzleVec8x16; }
 
+BinaryenExpressionRef BinaryenConstInt32(BinaryenModuleRef module, int32_t x) { return BinaryenConst(module, BinaryenLiteralInt32(x)); }
+BinaryenExpressionRef BinaryenConstInt64(BinaryenModuleRef module, int64_t x) { return BinaryenConst(module, BinaryenLiteralInt64(x)); }
+BinaryenExpressionRef BinaryenConstFloat32(BinaryenModuleRef module, float x) { return BinaryenConst(module, BinaryenLiteralFloat32(x)); }
+BinaryenExpressionRef BinaryenConstFloat64(BinaryenModuleRef module, double x) { return BinaryenConst(module, BinaryenLiteralFloat64(x)); }
+BinaryenExpressionRef BinaryenConstVec128(BinaryenModuleRef module, const uint8_t x[16]) { return BinaryenConst(module, BinaryenLiteralVec128(x)); }
+BinaryenExpressionRef BinaryenConstFloat32Bits(BinaryenModuleRef module, int32_t x) { return BinaryenConst(module, BinaryenLiteralFloat32Bits(x)); }
+BinaryenExpressionRef BinaryenConstFloat64Bits(BinaryenModuleRef module, int64_t x) { return BinaryenConst(module, BinaryenLiteralFloat64Bits(x)); }
+
 BinaryenExpressionRef BinaryenBlock(BinaryenModuleRef module,
                                     const char* name,
                                     BinaryenExpressionRef* children,
@@ -4157,6 +4165,14 @@ char* BinaryenModuleAllocateAndWriteText(BinaryenModuleRef module) {
   char* cout = (char*)malloc(len);
   strncpy(cout, out.c_str(), len);
   return cout;
+}
+
+void BinaryenModuleAllocateAndWriteMut(BinaryenModuleRef module, const char* sourceMapUrl,
+  void** binary, size_t* binaryBytes, char** sourceMap) {
+  BinaryenModuleAllocateAndWriteResult r = BinaryenModuleAllocateAndWrite(module, sourceMapUrl);
+  *binary = r.binary;
+  *binaryBytes = r.binaryBytes;
+  *sourceMap = r.sourceMap;
 }
 
 BinaryenModuleRef BinaryenModuleRead(char* input, size_t inputSize) {
