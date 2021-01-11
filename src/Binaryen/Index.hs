c57c6697d31d18c188bd909d4f82b968fad31d6b
Cheng Shao <cheng.shao@tweag.io>
2021-01-11 12:57:00 +0000

    Enum & Integral instances for Index

--- src/Binaryen/Index.hs
+++ src/Binaryen/Index.hs
@@ -1,17 +1,16 @@
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 -- | The Binaryen index type.
 --
 -- See <https://github.com/WebAssembly/binaryen/blob/master/src/binaryen-c.h>
 -- for API documentation.
 --
 -- This module is intended to be imported qualified.
-
-{-# LANGUAGE DerivingStrategies #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-
 module Binaryen.Index where
 
 import Data.Word (Word32)
 import Foreign (Storable)
 
 newtype Index = Index Word32
-  deriving newtype (Eq, Num, Show, Storable)
+  deriving newtype (Enum, Eq, Integral, Num, Show, Storable)
